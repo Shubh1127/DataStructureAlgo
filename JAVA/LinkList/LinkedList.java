41646aaab3c29f3cf9c6211dcfa06ef176213aed
shubham <shubhamsinghmor2312@gmail.com>
2024-10-15 00:17:48 +0530

    changes done

--- JAVA/LinkList/LinkedList.java
+++ JAVA/LinkList/LinkedList.java
@@ -11,6 +11,21 @@ public class LinkedList{
     public static Node head;
     public static Node tail;
 
+    public void addFirst(int data){
+        //Step 1 =create new Node
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return; 
+        }
+
+        //Step 2 -newNode next=head
+        newNode.next=head.next; 
+        
+        //setp 3 -head =newNode
+        head=newNode;
+    }
+
 
 public static void main(String args[]){
     LinkedList ll=new LinkedList();
