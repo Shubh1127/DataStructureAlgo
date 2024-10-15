94de278c225161eee794bee0f40d9ddfb222d3c9
shubham <shubhamsinghmor2312@gmail.com>
2024-10-15 23:13:36 +0530

    added binomial coefficient code

--- JavaProgt/LinkList/Factorial.java
+++ JavaProgt/LinkList/Factorial.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Factorial {
-    public static int facto(int number){
+    public static double facto(double number){
         int fact=1;
-        for(int i=2;i<=number;i++){
+        for(int i=1;i<=number;i++){
             fact*=i;
         }
         return fact;
@@ -13,6 +13,17 @@ public class Factorial {
         Scanner s=new Scanner(System.in);
         int number=s.nextInt();
         System.out.println(facto(number));
+            //Binomial Coefficient
+        System.out.println("Enter the n:");
+        double num1=s.nextInt();
+        System.out.println("Enter the r:");
+        double num2=s.nextInt();
+        double n=  facto(num1);
+        double r=facto(num2);
+        double num3=num1-num2;
+        double sub=facto(num3);
+        double Coefficient=n/(r*sub);
+        System.out.println("Binomial Coefficient :  "+Coefficient);
         
 
     }   
