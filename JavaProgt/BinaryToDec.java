5e021dd726df8ac704bf231c34d57c912d87fdae
shubham <shubhamsinghmor2312@gmail.com>
2024-10-16 22:32:50 +0530

    added deci to bin code

--- JavaProgt/BinaryToDec.java
+++ JavaProgt/BinaryToDec.java
@@ -10,10 +10,27 @@ public class BinaryToDec {
         }
         return DecNum;
     }
+    public static int DectoBin(int n){
+        int pow=0,rem,BinaryNum=0;
+        while(n>0){
+            rem=n%2;
+            BinaryNum=BinaryNum+(rem*(int)Math.pow(10,pow));
+            pow++;
+            n/=2;
+        }
+        return BinaryNum;
+    }
     public static void main(String args[]){
+        // binary to decimal
         System.out.println("Enter a binary number");
         Scanner s=new Scanner(System.in);
         int binNum=s.nextInt();
-        System.out.println(BinToDec(binNum));
+        System.out.println("Decimal equivalent of "+binNum+" is :"+BinToDec(binNum));
+
+
+        //Decimal to binary
+        System.out.println("Enter a Decimal number ");
+        int Decum=s.nextInt();
+        System.out.println("Binary equivalent of "+Decum+" is : "+DectoBin(Decum));
     }
 }
