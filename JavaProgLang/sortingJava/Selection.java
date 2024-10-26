ac95c55676dba777c4f08580f06164114f3bfc59
shubham <shubhamsinghmor2312@gmail.com>
2024-10-26 21:08:08 +0530

    selection sort

---
+++ JavaProgLang/sortingJava/Selection.java
@@ -0,0 +1,20 @@
+public class Selection {
+    public static void main(String agrs[]){
+            int arr[]={5,1,3,2,6};
+
+            for(int i=0;i<arr.length-1;i++){
+                int minPos= i;
+                for(int j=i+1;j<arr.length;j++){
+                        if(arr[minPos]>arr[j]){
+                            minPos=j;   
+                        }
+                    }
+                        int temp=arr[minPos];
+                        arr[minPos]=arr[i];
+                        arr[i]=temp;
+            }
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+" ");
+            }
+    }
+}
