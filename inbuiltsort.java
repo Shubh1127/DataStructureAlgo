b165d41ab338d023ab70dc639edc3cb401f618c7
shubham <shubhamsinghmor2312@gmail.com>
2024-10-28 19:03:15 +0530

    added reverse order inbuilt collection

--- inbuiltsort.java
+++ inbuiltsort.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
+import java.util.Collections;
 public class inbuiltsort {
     public static void main(String args[]){
-        int arr[]={5,4,1,2,3};
-        Arrays.sort(arr);
+        Integer arr[]={5,4,1,2,3};
+        // Arrays.sort(arr);
+        Arrays.sort(arr,Collections.reverseOrder());
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
