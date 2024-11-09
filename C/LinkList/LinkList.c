f1813e09eed9a8894ba6a94c9398c8fa372f287c
shubham <shubhamsinghmor2312@gmail.com>
2024-11-09 22:08:51 +0530

    added link list code

--- C/LinkList/LinkList.c
+++ C/LinkList/LinkList.c
@@ -5,50 +5,74 @@ struct Node {
     int data;
     struct Node *next;
 };
-struct Node *start=NULL;
+struct Node *start;
 struct Node *insert_at_beg(struct Node*);
-void printList(struct Node *start){
-    struct Node *ptr;
-    ptr=start;
-    if(ptr==NULL){
-        printf("\nList is empty");
-    }else{
-        while(ptr!=NULL){
-        printf("|%d|->",ptr->data);
-        ptr=ptr->next;
-        }
-        printf("NULL\n");
-    }
-}
+struct Node *insert_at_end(struct Node*);
+void printData(struct Node*);
 int main(){
     int choice;
     char ch;
     do{
         printf("\n1.Insert at beginning");
+        printf("\n2.Insert at end");
         printf("\nEnter your choice");
         scanf("%d",&choice);
         switch(choice){
             case 1:start=insert_at_beg(start);
             break;
-            default:printf("\nWrong choice by user");
+            case 2:start=insert_at_end(start);
+            break;
+            case 3:printData(start);
+            break;
+            default:printf("\nwrong choice by user");
         }
-        printf("\nDo you want to continue(y/n)");
+        printf("\nDo you want to continue(y/n)?");
         ch=getche();
-    }while(ch=='y' || ch=='Y');
+    }while(ch=='y' || ch== 'Y');
 }
 struct Node *insert_at_beg(struct Node *start){
-    
-        struct Node *temp;
-        temp=(struct Node*)malloc(sizeof(struct Node));
-        printf("\nEnter the data");
-        scanf("%d",&temp->data);
+    struct Node *temp;
+    temp=(struct Node*)malloc(sizeof(struct Node));
+    printf("\nEnter the data");
+    scanf("%d",&temp->data);
     if(start==NULL){
         start=temp;
         temp->next=NULL;
-    }else{
+    }
+    else{
         temp->next=start;
         start=temp;
     }
-    printList(start);
-return start;
+    printData(start);
+    return start;
+}
+
+struct Node *insert_at_end(struct Node *start){
+    struct Node *temp;
+    temp=(struct Node*)malloc(sizeof(struct Node));
+    printf("\nEnter the data\n");
+    scanf("%d",&temp->data);
+    temp->next=NULL;
+    if(start==NULL){
+        start=temp;
+    }else{
+        struct Node *ptr;
+        ptr=start;
+        while(ptr->next!=NULL){
+            ptr=ptr->next;
+        }
+        ptr->next=temp;
+    }
+    printData(start);
+    return start;
+}
+void printData(struct Node *start){
+    struct Node *ptr;
+    ptr=start;
+    printf("|start|->");
+    while(ptr!=NULL){
+        printf("|%d|->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL");
 }
