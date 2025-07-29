0451f1ec098a3b5adb27294ee2e61ab04ebda4e9
Shubh1127 <shubahmsinghmor2312@gmail.com>
2025-07-29 21:58:18 +0530

    done

--- JavaProgLang/ArrayJava/TrappingRainWater.java
+++ JavaProgLang/ArrayJava/TrappingRainWater.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class TrappingRainWater{
-    
+    public static int trap(int height[]){
+        //calculate left max boundary
+        int n=height.length;
+        int leftMax[]=new int[n];
+        leftMax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftMax[i]=Math.max(leftMax[i-1],height[i]);
+        }
+        //calculate right max boundary
+        //loop
+        //waterlevel=min(leftmax,rightmax)
+        //trappedWater=waterLevel-height[i]
+            return 0;
+    }
+    public static void main(String args[]){
+        int height[]={4,2,0,6,3,2,5};
+    }
 }
