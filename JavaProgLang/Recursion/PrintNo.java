a576b7b4d1f017c8452ac7168d82b6b40c207343
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-08-27 22:01:36 +0530

    added printInc function

--- JavaProgLang/Recursion/PrintNo.java
+++ JavaProgLang/Recursion/PrintNo.java
@@ -7,9 +7,19 @@ public class PrintNo {
         System.out.print(n+" ");
         printDec(n-1);
     }
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String args[]){
         int n=10;
         printDec(n);
+        System.out.println();
+        printInc(n);
 
     }
 }
