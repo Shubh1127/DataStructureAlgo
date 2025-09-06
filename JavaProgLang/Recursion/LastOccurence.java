cae784796cebd2a3f56ee5433aeaaef3abe1844c
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-09-06 23:49:24 +0530

    done

--- JavaProgLang/Recursion/LastOccurence.java
+++ JavaProgLang/Recursion/LastOccurence.java
@@ -10,7 +10,7 @@ public class LastOccurence {
         return isFound;
     }
  public static void main(String args[]){
-    int arr[]={4,3,80,7,9,4,5,6,7,0,1,10,20};
+    int arr[]={4,3,80,7,9,4,5,6,7,0,1,10,100};
     int key=4;
     System.out.println("Last occurrence of "+key+" is at index : "+lastOccurence(arr,key,0));
 
