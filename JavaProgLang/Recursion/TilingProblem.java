9f7d106f998ac6ae8d4dde11a2be301736c7e3bf
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-09-07 23:29:55 +0530

    completed tilling problem

---
+++ JavaProgLang/Recursion/TilingProblem.java
@@ -0,0 +1,15 @@
+public class TilingProblem {
+
+    public static int countWays(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return countWays(n - 1) + countWays(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // Example: 4x1 wall
+        System.out.println("Number of ways to tile " + n + "x1 wall: " + countWays(n));
+    }
+    
+}
