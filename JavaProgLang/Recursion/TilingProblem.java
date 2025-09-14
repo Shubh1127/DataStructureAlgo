86396396447768b4c1b76726de4aacc75bb7774a
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-09-14 23:12:44 +0530

    done

--- JavaProgLang/Recursion/TilingProblem.java
+++ JavaProgLang/Recursion/TilingProblem.java
@@ -8,7 +8,7 @@ public class TilingProblem {
     }
 
     public static void main(String[] args) {
-        int n = 4; // Example: 4x1 wall
+        int n = 4; // Example: 4x1 tiles
         System.out.println("Number of ways to tile " + n + "x1 wall: " + countWays(n));
     }
     
