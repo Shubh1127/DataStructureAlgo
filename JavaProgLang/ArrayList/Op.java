e34b99a8a8c052fa040769621566e163cf24f9f9
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-12-23 23:18:30 +0530

    added operation on ArrayList

--- JavaProgLang/ArrayList/Op.java
+++ JavaProgLang/ArrayList/Op.java
@@ -5,7 +5,26 @@ import java.util.ArrayList;
 public class Op {
     public static void main(String args[]){
         ArrayList<Integer> list=new ArrayList<>();
+        //Add operation
         list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        //Get operation
+        list.get(2);
         System.out.println(list);
+        System.out.println(list.get(2));
+
+        //Remove operation
+        list.remove(2);
+        System.out.println(list);
+
+        //Set operation
+        list.set(2,10);
+        System.out.println(list);
+
+        //Contains operation
+        System.out.println(list.contains(10));
     }
 }
