9b12a05298062047e513046828245c92e1cb7f5a
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-12-23 23:13:33 +0530

    started ArrayList

---
+++ JavaProgLang/ArrayList/Op.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+//operations on ArrayList
+
+public class Op {
+    public static void main(String args[]){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
