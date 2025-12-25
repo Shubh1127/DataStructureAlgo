850358056eb9b467324b5bcd415719a283417d1f
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-12-25 20:49:57 +0530

    added two methods of linkedlist to insert data

--- JavaProgLang/LinkList/LinkedList.java
+++ JavaProgLang/LinkList/LinkedList.java
@@ -17,14 +17,36 @@ public class LinkedList{
     Node newNode=new Node(data);
     if(head==null){
         head=tail=newNode;
+        print(head);
         return;
     }
     newNode.next=head;
     head=newNode;
-    
+    print(head);
+   }
+   public static void addLast(int data){
+    Node newNode=new Node(data);
+    if(head==null){
+        addFirst(data);
+        return;
+    }
+    tail.next=newNode;
+    tail=newNode;
+    print(head);
+   }
+   public static void print(Node head){
+    Node temp=head;
+    while(temp!=null){
+        System.out.print(temp.data+" ");
+        temp=temp.next;
+    }
+    System.out.println();
    }
    public static void main(String args[]){
     LinkedList ll=new LinkedList();
+    ll.addFirst(2);
+    ll.addFirst(1);
+    ll.addLast(3);
 
    }
 }
