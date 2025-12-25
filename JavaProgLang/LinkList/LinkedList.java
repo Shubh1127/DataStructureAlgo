d2f3fac027421fa7a718069247bb2d40c3cc11b2
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-12-25 20:40:46 +0530

    added code for linkedlist

--- JavaProgLang/LinkList/LinkedList.java
+++ JavaProgLang/LinkList/LinkedList.java
@@ -1,37 +1,30 @@
 
 
 public class LinkedList{
-    public static class Node{
-        int data;
-        Node next;
+   public static class Node{
+    int data;
+    Node next;
 
-        public Node(int data){
-            this.data=data;
-            this.next=null;
-        }
+    public Node(int data){
+        this.data=data;
+        this.next=null;
     }
-    public static Node head;
-    public static Node tail;
+   }
+   public static Node head;
+   public static Node tail;
 
-    public void addFirst(int data){
-        //Step 1 =create new Node
-        Node newNode=new Node(data);
-        if(head==null){     
-            head=tail=newNode;
-            return; 
-        }
-
-        //Step 2 -newNode next=head
-        newNode.next=head.next; 
-        
-        //setp 3 -head =newNode
-        head=newNode;
+   public static void addFirst(int data){
+    Node newNode=new Node(data);
+    if(head==null){
+        head=tail=newNode;
+        return;
     }
-
-
-public static void main(String args[]){
+    newNode.next=head;
+    head=newNode;
+    
+   }
+   public static void main(String args[]){
     LinkedList ll=new LinkedList();
 
-
-}
+   }
 }
