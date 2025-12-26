1a31c54c76e51336cced1e9d5aeb7b973dbf2c0a
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-12-26 20:43:20 +0530

    added full code for singly linked list

--- JavaProgLang/LinkList/LinkedList.java
+++ JavaProgLang/LinkList/LinkedList.java
@@ -30,17 +30,21 @@ public class LinkedList{
    }
    public static Node addLast(Node head){
     System.out.println("Enter data to add at last: ");
-    int data=s.nextInt();
-    Node newNode=new Node(data);
-    if(head==null){
-        return addFirst(head);
-        
+    int data = s.nextInt();
+    Node newNode = new Node(data);
+
+    if(head == null){
+        head = tail = newNode;
+        print(head);
+        return head;
     }
-    tail.next=newNode;
-    tail=newNode;
+
+    tail.next = newNode;
+    tail = newNode;
     print(head);
     return head;
-   }
+}
+
    public static void print(Node head){
     Node temp=head;
     System.out.print("[Head]->");
@@ -53,27 +57,89 @@ public class LinkedList{
     }
     System.out.println();
    }
+   //add at mid
    public static Node add_at_mid(Node head){
     
     if(head==null){
         return  addFirst(head);
-    }else if(head.next==null){
-        return addLast(head);
     }
     print(head);
     System.out.println("Enter the data after which you want to add new node: ");
     int afterdata=s.nextInt();
-    System.out.println("Enter data to add at mid: ");
-    int data=s.nextInt();
-    Node newNode=new Node(data);
+    
+    
     Node temp=head;
     while(temp!=null && temp.data!=afterdata){
         temp=temp.next;
     }
+    if(temp==null){
+        System.out.println("Node with data "+afterdata+" not found.");
+        return head;
+    }
+    System.out.println("Enter data to add at mid: ");
+    int data=s.nextInt();
+    Node newNode=new Node(data);
+
     newNode.next=temp.next;
     temp.next=newNode;
+
+    if(temp==tail){
+        tail=newNode;
+    }
     print(head);
     return head;
+   }
+
+   public static Node removeFirst(Node head){
+    if(head==null){
+        System.out.println("List is empty");
+        return head;
+    }else if(head.next==null){
+        head=tail=null;
+    }else{
+        head=head.next;
+    }
+    print(head);
+    return head;
+    
+   }
+   public static Node removeLast(Node head){
+    if(head==null){
+        return removeFirst(head);
+    }else if(head.next==null){
+        return removeFirst(head);
+    }else{
+        Node temp=head;
+        Node prev=null;
+        while(temp.next!=null){
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=null;
+        tail=prev;
+        print(head);
+        return head;
+    }
+    }
+    //remove at mid
+    public static Node remove_at_mid(Node head){
+        if(head==null){
+            return removeFirst(head);
+    }else if(head.next==null){
+        return removeFirst(head);
+    }else{
+        print(head);
+        System.out.println("Enter the data after which you want to remove node: ");
+        int afterdata=s.nextInt();
+        Node temp=head;
+        while(temp!=null && temp.data!=afterdata){
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
+        print(head);
+        return head;
+    }
+
    }
    public static void main(String args[]){
     // LinkedList ll=new LinkedList();
@@ -82,7 +148,7 @@ public class LinkedList{
     // ll.addLast(3);
     int n;
     System.out.println("Linked List Operations:");
-    System.out.println("1. Add First\n2. Add Last\n3. Add at Mid\n4. Print List");
+    System.out.println("1. Add First\n2. Add Last\n3. Add at Mid\n4. Print List\n5. Remove First\n6. Remove Last\n7. Remove at Mid\n8. Exit");
     while(true){
         System.out.println("Enter your choice: ");
         n=s.nextInt();
@@ -95,7 +161,16 @@ public class LinkedList{
                 break;
             case 3:head=add_at_mid(head);
                 break;
-            case 4:print(head);
+            case 4:head=removeFirst(head);
+                break;
+            case 5:head=removeLast(head);
+                break;
+            case 6:head=remove_at_mid(head);
+                break;
+            case 7:print(head);
+                break;
+            case 8:System.out.println("Exiting...");
+                System.exit(0);
                 break;
             default:
                 System.out.println("Invalid choice");
