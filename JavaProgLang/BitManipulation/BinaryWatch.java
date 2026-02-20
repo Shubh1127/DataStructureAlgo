4c6bfd85943fc4c8512796a566bba53d61a61b7b
shubham <shubhamsinghmor2312@gmail.com>
2026-02-20 21:53:39 +0530

    done

--- JavaProgLang/BitManipulation/BinaryWatch.java
+++ JavaProgLang/BitManipulation/BinaryWatch.java
@@ -12,6 +12,6 @@ public class BinaryWatch {
     }
     public static void main(String args[]){
         BinaryWatch obj=new BinaryWatch();
-        System.out.println(obj.readBinaryWatch(1));
+        System.out.println(obj.readBinaryWatch(2));
     }
 }
